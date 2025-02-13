6a64dcf715ddef093b06d4e71fae3a5eb2851ebe
Pulkit Sharma <Pulkit2005sharma@gmail.com>
2025-02-14 02:05:59 +0530

    restruct

--- apps/excelidrae-frontend/app/canvas/[roomId]/page.tsx
+++ apps/excelidrae-frontend/app/canvas/[roomId]/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useEffect,useRef } from "react";
+import {initDraw }from "@/draw"
 
 export default function Canvas() {
     const canvasRef = useRef(null);
@@ -7,43 +8,15 @@ export default function Canvas() {
     useEffect(()=> {
 
         if(canvasRef.current) {
-            const canvas = canvasRef.current;
-            const ctx = canvas.getContext("2d");
 
-            if(!ctx) {
-                return
-            }
-
-            let clicked = false;
-            let startX = 0;
-            let startY = 0;
-
-            canvas.addEventListener("mousedown", (e) => {
-                clicked = true;
-                startX = e.clientX
-                startY = e.clientY
-            })
-
-            canvas.addEventListener("mouseup", (e) => {
-                clicked = false;
-                console.log(e.clientX)
-                console.log(e.clientY)
-            })
-
-            canvas.addEventListener("mousemove", (e) => {
-                if(clicked) {
-                    const width = e.clientX - startX;
-                    const height = e.clientY - startY;   
-                    ctx.clearRect(0, 0, canvas.width, canvas.height);
-                    ctx.strokeRect(startX, startY, width, height);
-                }
-            })
+            initDraw(canvasRef.current);
+            
 
         }
 
 
     }, [canvasRef])
     return <div>
-        <canvas ref={canvasRef} width={500} height={500}></canvas>
+        <canvas ref={canvasRef} width={2000} height={1000}></canvas>
     </div>
 }
