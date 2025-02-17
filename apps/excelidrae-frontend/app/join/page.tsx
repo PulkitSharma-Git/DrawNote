330a9a648d13568d5c81dd51d894a5c1b3d69193
Pulkit Sharma <Pulkit2005sharma@gmail.com>
2025-02-17 22:10:50 +0530

    Ugly Signin and RoomMaking with bad alerts

---
+++ apps/excelidrae-frontend/app/join/page.tsx
@@ -0,0 +1,47 @@
+"use client"
+import { HTTP_BACKEND } from "@/config";
+import axios from "axios";
+
+import { useRef } from "react"
+import { useRouter } from "next/navigation";
+
+export default function Join() {
+    const roomref = useRef<HTMLInputElement>(null);
+    const router = useRouter();
+
+    async function createRoomHandler() {
+        if(roomref.current) {
+            const token = localStorage.getItem("token");
+
+            try{
+                console.log(token)
+                console.log(roomref.current.value)
+                const response = await axios.post(`${HTTP_BACKEND}/room`, {
+                    name: roomref.current.value
+                }, {
+                    headers: {
+                        "authorization": token
+                    }
+                } );
+
+                const roomId = response.data.roomId;
+                console.log(roomId)
+                router.push(`/canvas/${roomId}`);
+
+            }catch(e) {
+                alert("failed to create room");
+            }
+        } else {
+            alert("room name possibly null");
+        }
+        
+    }
+
+
+
+    return <div>
+        <input ref = {roomref} type="text" placeholder="room-name"/>
+        <button onClick={ () => createRoomHandler()}>Create room</button>
+    </div>
+
+}
